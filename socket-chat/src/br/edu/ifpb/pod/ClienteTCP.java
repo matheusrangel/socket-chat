a89ea66fcf6cb5541e13415930dee07344f2c78c
Victor Pereira <victordualib@gmail.com>
2015-12-16 20:30:42 -0300

    Finalizando o projeto

--- socket-chat/src/br/edu/ifpb/pod/ClienteTCP.java
+++ socket-chat/src/br/edu/ifpb/pod/ClienteTCP.java
@@ -1,29 +1,79 @@
 package br.edu.ifpb.pod;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.Socket;
 
-import javax.swing.JOptionPane;
-
-public class ClienteTCP {
-
-	public static void main(String[] args) throws IOException {
-		
-		Socket socket = new Socket("127.0.0.1", 1593);
-		DataInputStream entrada = new DataInputStream(socket.getInputStream());
-		DataOutputStream saida = new DataOutputStream(socket.getOutputStream());
-		String msg = new String();
-		
-		do {
-			String msgEnvio = JOptionPane.showInputDialog("Cliente, digite a msg: ");
-			System.out.println("Cliente: " + msgEnvio);
-			saida.writeUTF(msgEnvio);
-			msg = entrada.readUTF();
-			System.err.println("Servidor: " + msg);
-		} while (msg != "FECHAR");
-		socket.close();
-
-	}
-
+public class ClienteTCP extends Thread {
+    // parte que controla a recepcao de mensagens do cliente
+    private Socket conexao;
+    // construtor que recebe o socket do cliente
+    public ClienteTCP(Socket socket) {
+        this.conexao = socket;
+    }
+    public static void main(String args[])
+    {
+        try {
+            //Instancia do atributo cvonexao do tipo Socket, conecta a IP do Servidor, Porta
+            Socket socket = new Socket("127.0.0.1", 5555);
+            
+            //Instancia do atributo saida, obtem os objetos que permitem controlar o fluxo de comunicacao
+            PrintStream saida = new PrintStream(socket.getOutputStream());
+            BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+            
+            System.out.print("> Digite seu nome: ");
+            String meuNome = teclado.readLine();
+            
+            //envia o nome digitado para o servidor
+            saida.println(meuNome);
+            
+            //instancia a thread para ip e porta conectados e depois inicia ela
+            Thread thread = new ClienteTCP(socket);
+            thread.start();
+            
+            //Cria a variavel msg responsavel por enviar a mensagem para o servidor
+            String msg = null;
+            while (true)
+            {
+                // cria linha para digitacao da mensagem e a armazena na variavel msg
+            	System.out.print("> ");
+                msg = teclado.readLine();
+            	
+                // envia a mensagem para o servidor
+                saida.println(msg);
+            }
+        } catch (IOException e) {
+            System.out.println("Falha na Conexao... .. ." + " IOException: " + e);
+        }
+    }
+    // execucao da thread
+    public void run()
+    {
+        try {
+            //recebe mensagens de outro cliente atraves do servidor
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(this.conexao.getInputStream()));
+            //cria variavel de mensagem
+            String msg;
+            while (true)
+            {
+                // pega o que o servidor enviou
+                msg = entrada.readLine();
+                //se a mensagem contiver dados, passa pelo if, caso contrario cai no break e encerra a conexao
+                if (msg == null) {
+                    System.out.println("Conexao encerrada com o bate-papo!");
+                    System.exit(0);
+                }
+                //imprime a mensagem recebida
+                System.out.println(msg);
+                
+                //cria uma linha visual para resposta
+                System.out.print("> ");
+            }
+        } catch (IOException e) {
+            // caso ocorra alguma excecao de E/S, mostra qual foi.
+            System.out.println("Ocorreu uma Falha... .. ." + " IOException: " + e);
+        }
+    }
 }
--- socket-chat/src/br/edu/ifpb/pod/ServerTCP.java
+++ socket-chat/src/br/edu/ifpb/pod/ServerTCP.java
@@ -1,32 +1,136 @@
 package br.edu.ifpb.pod;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
-import javax.swing.JOptionPane;
+public class ServerTCP extends Thread {
+    private static Map<String, PrintStream> map_clientes;
+    private Socket conexao;
+    private String nomeCliente;
+    private static List<String> lista_de_usuarios = new ArrayList<String>();
 
-public class ServerTCP {
+    public ServerTCP(Socket socket) {
+        this.conexao = socket;
+    }
 
-	public static void main(String[] args) throws IOException {
-		ServerSocket serverSocket = new ServerSocket(1593, 10);
-		Socket socket = serverSocket.accept();
-		DataInputStream entrada = new DataInputStream(socket.getInputStream());
-		DataOutputStream saida = new DataOutputStream(socket.getOutputStream());
-		String msg = new String();
-		
-		do {
-			msg = entrada.readUTF();
-			System.err.println("Cliente: " + msg);
-			String msgEnvio = JOptionPane.showInputDialog("Servidor, digite a msg: ");
-			System.out.println("Servidor: " + msgEnvio);
-			saida.writeUTF(msgEnvio);
-		} while (msg != "SAIR");
-		
-		socket.close();
-		serverSocket.close();
+    public boolean armazena(String newName) {
+        for (int i = 0; i < lista_de_usuarios.size(); i++) {
+            if (lista_de_usuarios.get(i).equals(newName))
+                return true;
+        }
+        lista_de_usuarios.add(newName);
+        return false;
+    }
 
-	}
+    public void remove(String oldName) {
+        for (int i = 0; i < lista_de_usuarios.size(); i++) {
+            if (lista_de_usuarios.get(i).equals(oldName))
+                lista_de_usuarios.remove(oldName);
+        }
+    }
 
+    public static void main(String args[]) {
+        map_clientes = new HashMap<String, PrintStream>();
+        try {
+            ServerSocket server = new ServerSocket(5555);
+            System.out.println("SocketChat IFPB rodando na porta 5555!");
+            while (true) {
+                Socket conexao = server.accept();
+                Thread t = new ServerTCP(conexao);
+                t.start();
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+    }
+
+    public void run() {
+        try {
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(this.conexao.getInputStream()));
+            PrintStream saida = new PrintStream(this.conexao.getOutputStream());
+            this.nomeCliente = entrada.readLine();
+            if (armazena(this.nomeCliente)) {
+                saida.println("Este nome ja existe! Conecte novamente com outro nome.");
+                this.conexao.close();
+                return;
+            } else {
+                //mostra o nome do cliente conectado ao servidor
+                System.out.println(this.conexao.getInetAddress() + ":" + this.conexao.getPort() + "/~"+ 
+				this.nomeCliente +": conectado ao bate-papo!");
+            }
+
+            if (this.nomeCliente == null) {
+                return;
+            }
+            
+            //adiciona os dados de saida do cliente no objeto map_clientes
+            //A chave sera o nome e valor o printstream
+            map_clientes.put(this.nomeCliente, saida);
+
+            String msg = entrada.readLine();
+            
+            while (msg != null && !(msg.trim().equals(""))) {
+            	String[] cmd = msg.split(" ");
+            	
+            	if(msg.equals("bye")){
+            		System.out.println(this.conexao.getInetAddress() + ":" + this.conexao.getPort() + "/~"+ 
+                            this.nomeCliente +" saiu do bate-papo!");
+                    remove(this.nomeCliente);
+                    map_clientes.remove(this.nomeCliente);
+                    this.conexao.close();
+            	}else if(msg.equals("list")){
+                	saida.println("Conectados: " + lista_de_usuarios.toString());
+                }
+            	else{
+                	switch(cmd[0].trim()){
+                	case "send":
+                		send(saida, cmd);
+                		break;
+                	case "rename":
+//                		rename(saida, cmd);
+                		break;
+                	default:
+                		saida.println("Comando invalido!");
+                	}
+                }
+            	
+            	msg = entrada.readLine();
+            }
+	
+        } catch (IOException e) {
+            System.out.println("Falha na Conexao... .. ." + " IOException: " + e);
+        }
+    }
+
+    public void send(PrintStream saida, String[] msg) {
+    	SimpleDateFormat formatador =
+    			new SimpleDateFormat("HH:mm dd/MM/yyyy");
+    	
+    	GregorianCalendar d = new GregorianCalendar();
+    	String datahora = formatador.format(d.getTime());
+    		
+    	for (Map.Entry<String, PrintStream> cliente : map_clientes.entrySet()) {
+    		PrintStream chat = cliente.getValue();
+    		if(chat != saida){
+    			if(msg.length == 3){
+    				chat.println(this.conexao.getInetAddress() + ":" + this.conexao.getPort() + "/~"+ 
+    						this.nomeCliente + ": " +msg[2]+ " "+ datahora);
+    			}else{
+    				if(msg[2].equalsIgnoreCase(cliente.getKey())){
+    					chat.println(this.conexao.getInetAddress() + ":" + this.conexao.getPort() + "/~"+ 
+        						this.nomeCliente + ": " +msg[3]+ " "+ datahora);
+        				break;
+    				}
+    			}
+    		}
+    		
+    	}	
+    }
+    
 }
